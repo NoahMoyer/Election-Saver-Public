b81a9b8574cfde30179e208281335ae08b755919
Nathanael Moyer <36149055+nathanaelmoyer@users.noreply.github.com>
2023-09-27 14:59:33 -0400

    Fixed update checker
    With move to new public github the link to check for an available update has changed but the link being referenced in the program was old.

--- Election Saver/Election Saver/Update.cs
+++ Election Saver/Election Saver/Update.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Diagnostics;
+using System.Windows;
 
 namespace Access_Time_Analyzer
 {
@@ -17,35 +18,67 @@ namespace Access_Time_Analyzer
         /// </summary>
         public Update()
         {
-            ///getting version on github
-            System.Net.WebClient wc = new System.Net.WebClient();
-            string webData = wc.DownloadString("https://raw.githubusercontent.com/NoahMoyer/Election-file-saver/main/Election%20Saver/Election%20Saver/Properties/AssemblyInfo.cs");
 
-            //parse webData for the following line:
-            //[assembly: AssemblyVersion("1.2.2")]
-            var versionLineRegex = Regex.Match(webData, @"AssemblyVersion\s*\(\s*""([0-9\.]*?)""\s*\)");
-            string versionLineString = versionLineRegex.ToString();
-            string[] versionArray = versionLineString.Split('\"');
-            //string onlineVersion should be the following using the previous example:
-            //1.2.2
-            int[] onlineVersion = versionArray[1].Split('.').Select(n => Convert.ToInt32(n)).ToArray(); ;
+            //Begin logging info
+            string logFile = "log.txt";
+            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            //End logging info
 
-            ///getting version from local system
-            //This function is similar to the above. Only difference is we don't have to parse the file.
-            //localVersionVar is simply returning the version number reported by the assembler
-            var localVersionVar = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
-            string localVersionString = localVersionVar.ToString();
-            int[] localVersion = localVersionString.Split('.').Select(n => Convert.ToInt32(n)).ToArray();
 
-            //compare version numbers
-            for (int i = 0; onlineVersion.Length > i; i++)
+            /// <summary>
+            /// Function to standardize logging data format.
+            /// </summary>
+            async void Log(string logData)
             {
-                //if version on github is greater than the local version return true
-                if (onlineVersion[i] > localVersion[i])
+                try
                 {
-                    updateAvail = true;
+                    File.AppendAllText(logFile, "[" + DateTime.Now + "] " + "[User name: " + userName + "] Log: " + logData + "\n");
+                }
+                catch (Exception Error)
+                {
+                    MessageBox.Show("Failed to write log file. Program should function like normal.\n\n" + Error, "\n\nInformation", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+
+            }
+
+            try
+            {
+                ///getting version on github
+                System.Net.WebClient wc = new System.Net.WebClient();
+                string webData = wc.DownloadString("https://raw.githubusercontent.com/NoahMoyer/Election-Saver-Public/main/Election%20Saver/Election%20Saver/Properties/AssemblyInfo.css");
+
+                //parse webData for the following line:
+                //[assembly: AssemblyVersion("1.2.2")]
+                var versionLineRegex = Regex.Match(webData, @"AssemblyVersion\s*\(\s*""([0-9\.]*?)""\s*\)");
+                string versionLineString = versionLineRegex.ToString();
+                string[] versionArray = versionLineString.Split('\"');
+                //string onlineVersion should be the following using the previous example:
+                //1.2.2
+                int[] onlineVersion = versionArray[1].Split('.').Select(n => Convert.ToInt32(n)).ToArray(); ;
+
+                ///getting version from local system
+                //This function is similar to the above. Only difference is we don't have to parse the file.
+                //localVersionVar is simply returning the version number reported by the assembler
+                var localVersionVar = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+                string localVersionString = localVersionVar.ToString();
+                int[] localVersion = localVersionString.Split('.').Select(n => Convert.ToInt32(n)).ToArray();
+
+                //compare version numbers
+                for (int i = 0; onlineVersion.Length > i; i++)
+                {
+                    //if version on github is greater than the local version return true
+                    if (onlineVersion[i] > localVersion[i])
+                    {
+                        updateAvail = true;
+                    }
                 }
             }
+            catch (Exception Error) 
+            {
+                Log("Failed to check for application update. Program should function like normal. Cath error:\n " + Error);
+                //MessageBox.Show("Failed to check for application update. Program should function like normal.\n\n" + Error, "\n\nInformation", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
 
 
         }

0c537c7ec45ed4fce0ec4daf4fc831e59d5a7cfb
Nathanael Moyer <36149055+nathanaelmoyer@users.noreply.github.com>
2024-02-27 09:30:17 -0500

    Fixed Click-Once-Error
    Certificate expired for the click once functionality so I disabled it as we are no longer using it. This issue was preventing the application from opening.

--- Election Saver/Updater/obj/Debug/net6.0-windows/Updater.AssemblyInfo.cs
+++ Election Saver/Updater/obj/Debug/net6.0-windows/Updater.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Updater")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.1.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Updater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Updater")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.1.1.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
